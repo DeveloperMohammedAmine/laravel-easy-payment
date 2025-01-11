fb4979eade2228b7b5e07b2ceb436c26a03d5d64
MohammedAmineDeveloper <developermohammed1997@gmail.com>
2025-01-11 22:10:42 +0100

    improve get credentials dynamically or form config code

--- app/Services/PaymentMethods/MyfatoorahPaymentMethod.php
+++ app/Services/PaymentMethods/MyfatoorahPaymentMethod.php
@@ -5,42 +5,38 @@ namespace App\Services\PaymentMethods;
 use App\Services\PaymentMethods\BasePaymentMethod;
 use App\interfaces\PaymentMethods\PaymentMethodsInterface;
 
-class MyfatoorahPaymentMethod extends BasePaymentMethod implements PaymentMethodsInterface{
+class MyfatoorahPaymentMethod extends BasePaymentMethod implements PaymentMethodsInterface {
 
     private $DynamicPaymentCredentials;
 
     private $api_key;
 
     public function __construct(array $credentials = null) {
-
         $this->DynamicPaymentCredentials = $credentials;
     }
 
-
     public function setCredentials() {
-
-        if($this->DynamicPaymentCredentials) {
-            $this->api_key = $this->DynamicPaymentCredentials['api_key'];
-            $this->test_mode_status = $this->DynamicPaymentCredentials['test_mode_status'];
-        }
-        
-        return $this;
+        $this->DynamicPaymentCredentials ? 
+            $this->setCredentialsDynamically() : 
+        $this->setCredentialsFromConfig();
     }
 
-    public function setDefaultCredentials() {
+    public function setCredentialsDynamically() {
+        $this->api_key = $this->DynamicPaymentCredentials['api_key'];
+        $this->test_mode_status = $this->DynamicPaymentCredentials['test_mode_status'];
+    }
+
+    public function setCredentialsFromConfig() {
         $this->api_key = config('easy-payment.myfatoorah.api_key');
-        $this->test_mode_status = config('easy-payment.myfatoorah.test_mode_status');
+        $this->test_mode_status = config('easy-payment.myfatoorah.test_mode_status');  
     }
 
     public function pay() {
 
-        if(!$this->DynamicPaymentCredentials) {
-            $this->setDefaultCredentials();
-        } else {
-            $this->setCredentials();
-        }
+        $this->setCredentials();
 
-        return  dd($this->test_mode_status);
+        return dd($this->api_key);
+
     }
 
 }
